f06dd8b6871c3ce3f2c5c19e4f4c70ed2878b9ba
Chris0024 <47217464+Chris0024@users.noreply.github.com>
2026-03-23 14:01:20 -0300

    Update mult.cpp

--- mult.cpp
+++ mult.cpp
@@ -1,6 +1,55 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <chrono>
+
 using namespace std;
-int main(){
-  int A[2][2];
-  return 0;
+using namespace std::chrono;
+
+int main() {
+    int MAX = 1000;
+
+    vector<vector<double>> A(MAX, vector<double>(MAX));
+    vector<double> x(MAX), y(MAX);
+
+    // Inicializar
+    for (int i = 0; i < MAX; i++) {
+        x[i] = i;
+        y[i] = 0;
+        for (int j = 0; j < MAX; j++) {
+            A[i][j] = i + j;
+        }
+    }
+
+    //  Caso 1:
+    auto start1 = high_resolution_clock::now();
+
+    for (int i = 0; i < MAX; i++) {
+        for (int j = 0; j < MAX; j++) {
+            y[i] += A[i][j] * x[j];
+        }
+    }
+
+    auto end1 = high_resolution_clock::now();
+    auto duration1 = duration_cast<milliseconds>(end1 - start1);
+
+    cout << "Tiempo (i-j): " << duration1.count() << " ms" << endl;
+
+    // Reset y
+    fill(y.begin(), y.end(), 0);
+
+   //  Caso 2:
+    auto start2 = high_resolution_clock::now();
+
+    for (int j = 0; j < MAX; j++) {
+        for (int i = 0; i < MAX; i++) {
+            y[i] += A[i][j] * x[j];
+        }
+    }
+
+    auto end2 = high_resolution_clock::now();
+    auto duration2 = duration_cast<milliseconds>(end2 - start2);
+
+    cout << "Tiempo (j-i): " << duration2.count() << " ms" << endl;
+
+    return 0;
 }
